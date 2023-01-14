d864b301e2a39fdaf1922a5e90c12f70157761b8
Eric <a365900630@gmail.com>
2023-01-15 02:25:03 +0900

    removed background on smaller screen, changed wordings

--- src/components/Introduction.js
+++ src/components/Introduction.js
@@ -8,10 +8,10 @@ function Introduction() {
       <div className="cs-briefing">
         <h3>What is Chaos Scroll?</h3>
         <h4>
-          &nbsp;&nbsp;&nbsp;&nbsp;Chaos Scroll 60%(CS) in The Game modifies an equipment to better or worse. 
+          &nbsp;&nbsp;&nbsp;&nbsp;Chaos Scroll 60% (CS) in Dream MS modifies an equipment to better or worse. 
           Upon passing, the equipment's stats can add any value between -5 and +5.
-          Enchanting with CS is risky, but it is the only way to make certain parts on equipment stronger, 
-          as these parts, such as shoes and rings, do not have scrolls which increase weapon attack or magic attack. 
+          Upgrading with CS is risky, but it is the only way to make certain equipments stronger, 
+          as they do not have scrolls which increase Weapon Attack or Magic Attack. 
         </h4>
         <h4>
           &nbsp;&nbsp;&nbsp;&nbsp;Try rolling as high attack value as possible!
--- src/components/Statistics.js
+++ src/components/Statistics.js
@@ -8,15 +8,15 @@ function Statistics() {
     <div className="statistics">
       <h3>A lil bit of context:</h3>
       <h4>
-        The Game uses White Scroll(ws) as currency for large transactions. White scroll prevents 
-        number of upgrades from decreasing in case of enchanting fails. Chaos Scrolls
-        only has 60% chance to pass, so most players use 1 Chaos scroll and 1 White Scroll per enchantment. 
+        Dream MS uses White Scroll (ws) as currency for large transactions. White scroll prevents 
+        number of upgrades from decreasing in case of fails. Chaos Scrolls
+        only have 60% chance to pass, so players use 1 Chaos scroll and 1 White Scroll per upgrade. 
       </h4>
       <h4>
-        A character can earn around 36 White Scrolls per week. Hardcore players create extra characters
+        A character can earn around 36 White Scrolls per week. Some players create extra characters
         in order to obtain more White Scrolls.
       </h4>
-      <h4>At the time of writing, 1 Chaos Scroll equals 0.8 ws, and the price of each equip is as follows:</h4>
+      <h4>At the time of writing, 1 cs equals 0.8 ws, and the price of each equipment is as follows:</h4>
       <ul>
         <li id="e1">Facestompers: 2.5 ws </li>
         <li id="e2">Blackfist Cloak: 0.8 ws</li>
@@ -24,20 +24,31 @@ function Statistics() {
         <li id="e4">OZ's Brilliant Belt: 15 ws</li>
         <li id="e5">Timeless Moonlight: ranging from 4ws to 8ws depending on initial stats</li>
       </ul>
-      <h4>Because Chaos Scrolls only have 60% chance to pass, upgrading each equipment max number of times would require (times / 0.6) sets of
+      <h4>Upgrading each equipment max number of times would require (+ Max / 0.6) sets of
         cs and ws. For example, Facestompers need 9 / 0.6 = 15 ws and cs sets. 15 cs equals 12 ws, so getting
         <strong> +9 Facestompers </strong> needs approximately 29.5 ws. The market price for 19 attack Facestompers is 30 ws, which 
-        means players want Facestompers to have higher than 19 attack to profit. 
+        means players want Facestompers to have equal to or higher than 19 attack to profit. 
       </h4>
-      <h4>However, what is the probability for it to happen? Below is the simulation of enchanting <strong>+9 Facestompers </strong> 
-        10 million times, with the assumption that players stop enchanting to move on to next when weapon attack is 4 or lower. 
+      <h4>For equipments with main stats, Total Weapon Attack is (Weapon Attack + the highest stat of (STR, LUK, DEX) / 4). 
+          Example: 10 weapon attack + 8 LUK = 12 weapon attack</h4>
+      <h4>1 INT equals 1 Magic Attack in Dream MS, so Total Magic Attack = Magic Attack + INT</h4>
+      <h4>Stats for each equipment (+ Max) needed to breakeven the cost: </h4>
+      <ul>
+        <li id="e1">Facestompers: 19 Weapon Attack </li>
+        <li id="e2">Blackfist Cloak: 19 Weapon Attack</li>
+        <li id="e3">Angelic Blessing: 12 Total Weapon Attack or 28 Total Magic Attack</li>
+        <li id="e4">OZ's Brilliant Belt: 9 Total Weapon Attack or 26 Total Magic Attack</li>
+        <li id="e5">Timeless Moonlight: 22 Total Weapon Attack or 40 Total Magic Attack</li>
+      </ul>
+      <h4>So, what is the probability to roll a 19 attack <strong>+9 Facestompers </strong>? Below is the simulation of upgrading <strong>+9 Facestompers </strong> 
+        10 million times, with the assumption that players stop upgrading to move on to next when weapon attack is 4 or lower. 
       </h4>
       <div><img src={fs_overall} alt='facestomper-enchanting-results' ></img></div>
       <h4>There is a 78% chance Facestompers will end up worse, but let's zoom in</h4>
       <div><img src={fs_zoomed} alt='facestomper-enchanting-results-5-or-higher'></img></div>
-      <h4>The cumulative probability of 20 or higher Facestompers is 4.8%.</h4>
+      <h4>The cumulative probability of 19 or higher Facestompers is 5.7%.</h4>
       <h4>Other equipments have similar distributions, in that there is at least 70% chance they will end up worse.</h4>
-      <h4>In conclusion, buying fully enchanted equipments from others is the prefered choice, but if one
+      <h4>In conclusion, buying + max equipments from others is the prefered choice, but if one
         were to defeat RNGesus and create a mega attack equipment, they would save a huge amount of expenses. A casino game.
       </h4>
     </div>
